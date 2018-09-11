9930147453d81c3626f5ecb183632dd0caa48959
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-11 17:26:51 +0100

    Test & Feature: add div for each new gift

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -17,3 +17,9 @@ it('adds a new gift to the state when clicking add gift button', () => {
   app.find('.btn-add').simulate('click');
   expect(app.state().gifts).toEqual([{id: 1}]);
 });
+
+it('adds a new gift to the list', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.find('.gift-list').children().length).toEqual(2); // test pollution
+});
---
+++ giftgiver/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render } from 'react-dom';
+
+import App from './components/App';
+
+// in public/index.html (created automatically)
+// there is a div with id of root enabled to render react app
+render(<App />, document.getElementById('root'))
