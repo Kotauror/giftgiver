c9439c18e55939972840e0fd4b6027b69de46709
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-11 16:22:53 +0100

    Feature & Test: add state to the app.

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -4,6 +4,11 @@ import App from './App';
 
 const app = shallow(<App />);
 
+// check if the app renders as we want it to.
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('initializes the states with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([]);
 });
